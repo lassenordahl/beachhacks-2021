ff00476ecdbef1d51f194e8bb7da384da9c351d5
Bernard Zhao <bernard.zhao.us@gmail.com>
2021-04-18 01:48:23 -0700

    Refactored playing

--- frontend/src/app/components/sequencer/Sequencer.js
+++ frontend/src/app/components/sequencer/Sequencer.js
@@ -2,7 +2,6 @@ import React, {useEffect, useState, useCallback} from "react";
 import "./Sequencer.scss";
 
 import * as Tone from "tone";
-import { Loop } from "tone";
 
 const default_sampler = new Tone.Sampler({
   urls: {
@@ -33,7 +32,6 @@ const NoteButton = ({ note, isActive, ...rest }) => {
 function Sequencer({ sampler=default_sampler, cols=8, disabled=false }) {
 
   const [grid, setGrid] = useState(GenerateGrid(cols));
-  const [isPlaying, setIsPlaying] = useState(false);
   const noteIndex = ["C", "D", "E", "G", "A"];
   const CHOSEN_OCTAVE = 1;
 
@@ -68,37 +66,6 @@ function Sequencer({ sampler=default_sampler, cols=8, disabled=false }) {
     return () => loop.dispose();
   }, [grid]);
 
-  const PlayMusic = async () => {
-    // Variable for storing our note in a appropriate format for our synth
-    let music = [];
-
-    grid.map((column) => {
-      let columnNotes = [];
-      column.map(
-        // boolean value if note should be played from grid
-        (shouldPlay, colIndex) =>
-          shouldPlay &&
-          columnNotes.push(noteIndex[colIndex] + CHOSEN_OCTAVE)
-      );
-      music.push(columnNotes);
-    });
-
-    // Starts our Tone context
-    await Tone.start();
-
-    if (isPlaying) {
-      // Turn of our player if music is currently playing
-      setIsPlaying(false);
-
-      await Tone.Transport.stop();
-
-      return;
-    }
-    setIsPlaying(true);
-    // Toggles playback of our musical masterpiece
-    await Tone.Transport.start();
-  };
-
   const handleNoteClick = (row, col) => {
     let copy = [...grid];
     copy[row][col] = copy[row][col] ? false : true;
@@ -113,9 +80,6 @@ function Sequencer({ sampler=default_sampler, cols=8, disabled=false }) {
           ))}
         </div>
       ))}
-      <button className="play-button" onClick={() => PlayMusic()}>
-        {isPlaying ? "Stop" : "Play"}
-      </button>
     </div>
   );
 }
