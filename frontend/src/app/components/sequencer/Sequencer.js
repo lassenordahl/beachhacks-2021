dfc2bf3bae17411ac6a49437a9e7fccc571b780b
WHellhaus <wolfgang.hellicksons@gmail.com>
2021-04-18 02:28:41 -0700

    don't know if this works, just need to merge

--- frontend/src/app/components/sequencer/Sequencer.js
+++ frontend/src/app/components/sequencer/Sequencer.js
@@ -36,39 +36,38 @@ function Sequencer({
   inputGrid,
 }) {
   const [grid, setGrid] = useState(inputGrid);
-  const [isPlaying, setIsPlaying] = useState(false);
   const noteIndex = ["C", "D", "E", "G", "A"];
   const CHOSEN_OCTAVE = 1;
 
-  useEffect(() => {
-    let music = [];
+  // useEffect(() => {
+  //   let music = [];
 
-    grid.map((column) => {
-      let columnNotes = [];
-      column.map(
-        // boolean value if note should be played from grid
-        (shouldPlay, colIndex) =>
-          shouldPlay && columnNotes.push(noteIndex[colIndex] + CHOSEN_OCTAVE)
-      );
-      music.push(columnNotes);
-    });
-    // Tone.Sequence()
-    //@param callback
-    //@param "events" to send with callback
-    //@param subdivision  to engage callback
-    let loop = new Tone.Sequence(
-      (time, column) => {
-        // Highlight column with styling
-        //setCurrentColumn(column);
+  //   grid.map((column) => {
+  //     let columnNotes = [];
+  //     column.map(
+  //       // boolean value if note should be played from grid
+  //       (shouldPlay, colIndex) =>
+  //         shouldPlay && columnNotes.push(noteIndex[colIndex] + CHOSEN_OCTAVE)
+  //     );
+  //     music.push(columnNotes);
+  //   });
+  //   // Tone.Sequence()
+  //   //@param callback
+  //   //@param "events" to send with callback
+  //   //@param subdivision  to engage callback
+  //   let loop = new Tone.Sequence(
+  //     (time, column) => {
+  //       // Highlight column with styling
+  //       //setCurrentColumn(column);
 
-        //Sends the active note to our Sampler
-        sampler.triggerAttackRelease(music[column], "8n", time);
-      },
-      [...Array(cols).keys()],
-      "8n"
-    ).start(0);
-    return () => loop.dispose();
-  }, [grid]);
+  //       //Sends the active note to our Sampler
+  //       sampler.triggerAttackRelease(music[column], "8n", time);
+  //     },
+  //     [...Array(cols).keys()],
+  //     "8n"
+  //   ).start(0);
+  //   return () => loop.dispose();
+  // }, [grid]);
 
   const handleNoteClick = (row, col) => {
     let copy = [...grid];
--- frontend/src/app/views/sequence/Sequence.js
+++ frontend/src/app/views/sequence/Sequence.js
@@ -11,10 +11,35 @@ function useQuery() {
 
 let colAmount = 20;
 
+const sampler1 = new Tone.Sampler({
+  urls: {
+    A1: "A1.mp3",
+  },
+  baseUrl: "https://tonejs.github.io/audio/casio/",
+}).toDestination();
+
+const sampler2 = new Tone.Sampler({
+  urls: {
+    A1: "A1.mp3",
+  },
+  baseUrl: "https://tonejs.github.io/audio/casio/",
+}).toDestination();
+
+const sampler3 = new Tone.Sampler({
+  urls: {
+    A1: "A1.mp3",
+  },
+  baseUrl: "https://tonejs.github.io/audio/casio/",
+}).toDestination();
+
 function Sequence() {
   const [sequenceData, setSequenceData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [isPlaying, setIsPlaying] = useState(false);
+  const samplers = [sampler1, sampler2, sampler3];
+  const noteIndex = ["C", "D", "E", "G", "A"];
+  const CHOSEN_OCTAVE = 1;
+
 
   let query = useQuery();
   let name = query.get("name");
@@ -27,6 +52,44 @@ function Sequence() {
 
   useEffect(() => {
     setLoading(false);
+    if(sequenceData) {
+      // hold individual music lists for each track/sequencer
+    let tracks = [];
+    sequenceData['sequence_data'].map((grid, gridIndex) => {
+      let music = [];
+
+      grid.map((column) => {
+        let columnNotes = [];
+        column.map(
+          // boolean value if note should be played from grid
+          (shouldPlay, colIndex) =>
+            shouldPlay && columnNotes.push(noteIndex[colIndex] + CHOSEN_OCTAVE)
+        );
+        music.push(columnNotes);
+      });
+
+      tracks.push(music);
+    })
+    console.log(tracks);
+    // Tone.Sequence()
+    //@param callback
+    //@param "events" to send with callback
+    //@param subdivision  to engage callback
+    let loop = new Tone.Sequence(
+      (time, column) => {
+        // Highlight column with styling
+        //setCurrentColumn(column);
+
+        //Sends the active note to our Sampler
+        tracks.map((music, trackIndex) => {
+          samplers[trackIndex].triggerAttackRelease(music[column], "8n", time);
+        })
+      },
+      [...Array(colAmount).keys()],
+      "8n"
+    ).start(0);
+    return () => loop.dispose();
+    }
   }, [sequenceData]);
 
   function loadSequenceData() {
