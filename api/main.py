8cb60fc943c25be09f63c4d48a1a06c331be970f
Lasse Nordahl <lasseanordahl@gmail.com>
2021-04-17 21:46:30 -0700

    api udpates

---
+++ api/main.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from fastapi import FastAPI, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="api/templates/static"), name="static")
+templates = Jinja2Templates(directory="api/templates")
+
+# Allow CORS since that's the main way we'll be interacting with the API
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
